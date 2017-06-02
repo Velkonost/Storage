0858a7bc0d86d080ed0793402869d57c23a72022
Artem <klimenkocpp@icloud.com>
2017-06-02 11:27:09 +0600

    added second db

--- models/Things.php
+++ models/Things.php
@@ -3,9 +3,14 @@
 namespace app\models;
 
 use yii\db\ActiveRecord;
+use Yii;
+
 
 class Things extends ActiveRecord {
-	
+	public static function getDb() {
+     	return Yii::$app->db;
+ 	}	
+
 }
 
 ?>
