fdc044b71866a768c7c97033d3266609a70f80f0
Artem <klimenkocpp@icloud.com>
2017-06-15 11:40:45 +0600

    ...

--- config/db.php
+++ config/db.php
@@ -5,5 +5,5 @@ return [
      'dsn' => 'mysql:host=localhost;dbname=rusich',
      'username' => 'sport_form',
      'password' => '!RsLkI0PBf4a0!',
-     'charset' => 'utf8',
+     'charset' => 'utf8', 
 ];
