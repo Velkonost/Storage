fb8cb3683786ccc4bc4be465928e63312ae8ed63
Artem <klimenkocpp@icloud.com>
2017-06-15 11:36:46 +0600

    changed db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=info664_storage',
-    'username' => '046487421_storag',
-    'password' => 'root',
-    'charset' => 'utf8',
+     'dsn' => 'mysql:host=localhost;dbname=rusich',
+     'username' => 'sport_form',
+     'password' => '!RsLkI0PBf4a0!',
+     'charset' => 'utf8',
 ];
